fc37b691289441eb3cdbb02a17efe4ad3fc9cd70
JayhouunC <jayhouunC@gmail.com>
2026-01-13 17:04:52 -0600

    count lines

--- src/main.ts
+++ src/main.ts
@@ -1,99 +1,45 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import { Plugin } from 'obsidian'
 
-// Remember to rename these classes and interfaces!
+export default class mdToRtfPlugin extends Plugin{
+	
+	statusBarTextElement: HTMLSpanElement;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	onload(){
+		this.readActiveFileAndUpdateLineCount();
+		this.statusBarTextElement = this.addStatusBarItem().createEl('span')
 
-	async onload() {
-		await this.loadSettings();
+		this.app.workspace.on('active-leaf-change', async () =>{
+			this.readActiveFileAndUpdateLineCount();
+		})
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
+		this.app.workspace.on('editor-change', editor =>{
+			const content = editor.getDoc().getValue();
+			this.updateLineCount(content);	
+		})
+		
+	}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	private async readActiveFileAndUpdateLineCount(){
+		const file = this.app.workspace.getActiveFile();
+		if(file){
+			const content = await this.app.vault.read(file);
+			this.updateLineCount(content);
+		}
+		else
+			this.updateLineCount(undefined);
+			
 
 	}
 
-	onunload() {
+	private updateLineCount(fileContent?: string){
+		const count = fileContent ? fileContent.split(/\r\n|\r|\n/).length : 0;
+		const linesWord = count === 1 ? "line" : "lines";
+		this.statusBarTextElement.textContent = `${count} ${linesWord}`;
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
-	}
+	
+	
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
 }
